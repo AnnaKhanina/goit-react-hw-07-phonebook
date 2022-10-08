c24ca6fbc90961cd949d04654ba66452ecb06149
Anna Khanina <77342940+AnnaKhanina@users.noreply.github.com>
2022-10-08 16:27:33 +0300

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,11 @@
-import { combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-const rootReducer = combineReducers({
-  filter: filterReducer,
-  contacts: contactsReducer,
-});
-
 export const store = configureStore({
-  reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+  reducer: {
+    filter: filterReducer,
+    contacts: contactsReducer,
+  },
 });
+   
